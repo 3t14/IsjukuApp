5419761ea508d217f763f88db689b37e13085280
Michiru Tanaka <michiru@r4hs.com>
2014-08-09 11:20:03 +0900

    First commit

---
+++ background.js
@@ -0,0 +1,11 @@
+/**
+ * @author Michiru Tanaka
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('window.html', {
+    'bounds': {
+      'width': 400,
+      'height': 480
+    }
+  });
+});
